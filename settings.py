e6b300713ab1aa7a8b81ec44fcd412a3de677be5
ljz2023 <136139984+ljz2023@users.noreply.github.com>
2023-06-10 11:56:46 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
